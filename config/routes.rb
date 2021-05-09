fb0186ed2e4fc91acf241945ef791bf2c2e455ee
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-05-09 20:27:37 +0300

    Add signup and log in functionalities, use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
+  root 'products#index'
+
   resources :products
   get 'dashboard' => 'products#dashboard'
-  
   get 'new_error_product' => 'products#new_error'
   post 'new_error_product' => 'products#new_error'
- 
-  get 'products/index'
-  root 'products#index'
+
+  resources :users
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
