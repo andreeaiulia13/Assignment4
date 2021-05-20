d7a8eceb31a0377d5731f7ffa1f0e0dd242c62be
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-05-20 21:55:59 +0300

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  host = 'food-ordering-system-ror.heroku.com'
+  config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
