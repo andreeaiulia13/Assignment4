e127c8e39048b30e59745a984d1e3a3d3cd00cf6
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-05-20 21:50:42 +0300

    Add mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'food-ordering-system-ror.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+  
+
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
