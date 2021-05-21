01495d50849e1dd250e89c48074cbe6445fb60ee
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-05-21 15:11:34 +0300

    Use nesting in scss, use a guard clause, avoid code repetion in rspec tests

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     patch :reduce_quantity, on: :member
   end
 
-  resources :account_activations, only: [:edit]
+  resources :account_activations, only: :edit
 
   resources :password_resets, only: %i[new create edit update]
 end
--- spec/requests/account_activations_spec.rb
+++ spec/requests/account_activations_spec.rb
@@ -2,29 +2,26 @@ require 'rails_helper'
 RSpec.describe 'Account Activations', type: :request do
   
   describe 'POST/sessions' do
+    subject {post login_path, params: {session: { email: user.email, password: user.password }}}
 
     context 'when account is not activated' do
-      let(:user) { User.create(name: 'Andreea', email: 'andreeaiulia@gmail.com', password: '123456', password_confirmation: '123456')}
-      
+      let(:user) { User.create(name: 'Example', email: 'example1@gmail.com', password: '123456', password_confirmation: '123456')}
+
       it 'redirect to main page with warning' do
-        post login_path, params: {session: { email: user.email, password: user.password }}
+        expect(subject).to redirect_to root_path 
         
-        expect(response).to redirect_to root_path 
         follow_redirect!
-        
         expect(response.body).to include('Account not activated.Check your email for the activation link')
         
         user.delete
       end
     end
-
+    
     context 'when account is activated' do 
-      let(:user) { User.create(name: 'Andreea', email: 'andreaexample11@gmail.com', password: '123456', password_confirmation: '123456', activated: true, activated_at: Time.now )}
-     
+      let(:user) { User.create(name: 'Example', email: 'example2@gmail.com', password: '123456', password_confirmation: '123456', activated: true, activated_at: Time.now)}
+      
       it 'redirect to user profile' do
-        post login_path, params: {session: { email: user.email, password: user.password }}
-        
-        expect(response).to redirect_to "/users/#{user.id}"
+        expect(subject).to redirect_to "/users/#{user.id}"
         
         user.delete
       end
