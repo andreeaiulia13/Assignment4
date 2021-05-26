b0d9fc88528f6c2a732131a83b954a00ee310f26
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-05-26 10:44:03 +0300

    Use ! at the end of the method because the method changes the internal state of the object

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  def create_reset_digest
+  def create_reset_digest!
     self.reset_token = User.new_token
     update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
